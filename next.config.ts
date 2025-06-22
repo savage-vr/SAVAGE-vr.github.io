2f3a512c3673dd4b50017a604a5be40bedf8adf5
MaxMEllon <maxmellon1994@gmail.com>
2025-06-22 22:42:33 +0900

    fix: configure

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  trailingSlash: true,
   images: { 
-    unoptimized: true 
+    unoptimized: true
   },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
 };
 
 export default nextConfig;
