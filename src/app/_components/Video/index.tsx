498abb7763650a01f63311ece9e7697d7d6619c9
MaxMEllon <maxmellon1994@gmail.com>
2025-10-16 16:07:26 +0900

    feat: use hls

--- src/app/_components/Video/index.tsx
+++ src/app/_components/Video/index.tsx
@@ -1,16 +1,26 @@
 'use client'
 
-import { useEffect, useState } from "react"
+import { useEffect, useState, useCallback } from "react"
 
 export default function Video() {
   const [load, setLoad] = useState(false)
+  const [animation, setAnimation] = useState(false)
+  const callback = useCallback(() => {
+    setAnimation(true)
+  }, [])
+  const ref = useCallback((node: HTMLVideoElement) => {
+    node?.addEventListener("loadstart", callback)
+  }, [])
   useEffect(() => {
     window.requestAnimationFrame(() => {
       setLoad(true)
     })
   }, [])
   if (!load) return null;
+  const anim = animation ? "animation" : ""
   return (
-    <video className="video fixed w-full h-screen items-center justify-center" src="/savage_bg.mp4" autoPlay muted loop />
+    <video ref={ref} className={`video fixed w-full h-screen items-center justify-center ${anim}`} autoPlay muted loop>
+      <source src="/movie/video.m3u8" />
+    </video>
   )
 }
